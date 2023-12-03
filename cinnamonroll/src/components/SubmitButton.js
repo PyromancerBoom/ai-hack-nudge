0aa56fb585ab79b4e164c7b3047519386da8fa85
PyromancerBoom <priyanshmagic@gmail.com>
2023-12-03 18:38:54 +0800

    css improve

--- cinnamonroll/src/components/SubmitButton.js
+++ cinnamonroll/src/components/SubmitButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const SubmitButton = () => {
   return (
     <div>
-      <button className="rounded-lg px-5 py-1 border-2 border-gray-700 text-white bg-gray-900 hover:bg-gray-700 hover:text-white duration-300">
+      <button className="rounded-lg px-3 py-0.5 border-1 border-gray-700 text-white bg-gray-900 hover:bg-gray-700 hover:text-white duration-300">
         Submit 🚀
       </button>
     </div>
