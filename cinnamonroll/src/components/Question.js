af25374f6bf997a9a56a8fbba6d9e9720e4eec17
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2023-12-05 03:16:41 +0800

    added Question.js, mapped promptGrader to Question

---
+++ cinnamonroll/src/components/Question.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import PromptGrader from "./PromptGrader";
+
+const Question = ({ question, index, handleButtonPress, isPressed }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div className="flex flex-col mt-4">
+      <h3 className="text-lg text-gray-300">Question {index + 1}:</h3>
+      <p className="text-base text-black-300 mt-2">{question}</p>
+      <PromptGrader />
+      <button
+        onClick={() => handleButtonPress(index)}
+        className={`rounded-lg px-3 py-0.5 border-1 border-gray-300 text-white ${
+          isPressed ? "bg-green-500" : "bg-gray-900"
+        } hover:bg-gray-700 hover:text-white duration-300`}
+      >
+        {isPressed ? "Done!" : "Satisfied?"}
+      </button>
+    </div>
+  );
+};
+
+export default Question;
